55632ba42bbe4329927e0d6612bf1b2f6e7340fd
Hye jun <vxggangxv@naver.com>
2017-02-02 08:58:19 +0900

    경로변경

--- src/js/app/index_app.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	/*var dh = $(document).height();
-	$("#backDrop").height(dh);*/
-	$("#fr_event_pop .clsBtn").on("click", function() {
-		$(this).closest("#fr_box").css("display", "none");
-		$("#backDrop").css("display", "none");
-	}); 
-});
